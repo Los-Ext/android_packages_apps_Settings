06aad514bb781cd540e44c40098a226ee78466fc
Ido Ben-Hur <idoybh2@gmail.com>
2020-10-03 15:43:31 +0300

    Settings: Add pref for qs brightness slider position [2/2]
    Change-Id: I583e652fad3e4f63182bab13a73188f35b8c4410
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    -----
    
    Settings: Allow hiding auto brightness toggle [2/2]
    
    Change-Id: Iafdae6d85062435f7b2cd8426c15dde1da1c923a
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>
    
    -----
    
    Settings: Observe auto brightness mode changes
    
    We have a toggle for that in QS. Reflect if the preferences are visible
    
    Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>
    Change-Id: I4d0a8198309568f963d6c9564bf08940f2869543
    Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

--- src/com/android/settings/lineage/fragments/quicksettings.java
+++ src/com/android/settings/lineage/fragments/quicksettings.java
@@ -16,20 +16,57 @@
 
 package com.android.settings.lineage.fragments;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.ContentResolver;
 import android.os.Bundle;
-import com.android.settings.SettingsPreferenceFragment;
-import com.android.settings.R;
+import android.provider.Settings;
+
+import androidx.preference.Preference;
+import androidx.preference.PreferenceScreen;
+import androidx.preference.Preference.OnPreferenceChangeListener;
+import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
 
-public class quicksettings extends SettingsPreferenceFragment {
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+import com.lineage.support.preferences.SecureSettingMasterSwitchPreference;
+
+public class quicksettings extends SettingsPreferenceFragment
+        implements Preference.OnPreferenceChangeListener {
 
     private static final String TAG = "Lineage Customizations";
+    private static final String BRIGHTNESS_SLIDER = "qs_show_brightness";
+
+    private SecureSettingMasterSwitchPreference mBrightnessSlider;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.lineage_quicksettings);
+        PreferenceScreen prefSet = getPreferenceScreen();
+        final ContentResolver resolver = getActivity().getContentResolver();
+
+        mBrightnessSlider = (SecureSettingMasterSwitchPreference)
+                findPreference(BRIGHTNESS_SLIDER);
+        mBrightnessSlider.setOnPreferenceChangeListener(this);
+        boolean enabled = Settings.Secure.getInt(resolver,
+                BRIGHTNESS_SLIDER, 1) == 1;
+        mBrightnessSlider.setChecked(enabled);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        ContentResolver resolver = getActivity().getContentResolver();
+        if (preference == mBrightnessSlider) {
+            boolean value = (boolean) newValue;
+            Settings.Secure.putInt(resolver,
+                    BRIGHTNESS_SLIDER, value ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 
     @Override
