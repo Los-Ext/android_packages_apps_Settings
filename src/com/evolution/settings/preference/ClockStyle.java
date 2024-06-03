yograjfire18/packages_apps_DerpSpace

Settings: Introduce Lockscreen Style [2/2]
* designed by Corex1402
* refactored by: rmp

Change-Id: Idb5fdf7af497c1e1a58f9cc04a591e4718389589
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

src/com/derp/settings/preference/ClockStyle.java
@@ -1,0 +1,79 @@
+package com.derp.settings.preference;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+import com.android.settings.R;
+
+public class ClockStyle extends RelativeLayout {
+
+    private static final int[] CLOCK_VIEW_IDS = {
+            R.id.keyguard_clock_style_default,
+            R.id.keyguard_clock_style_oos,
+            R.id.keyguard_clock_style_ios,
+            R.id.keyguard_clock_style_cos,
+            R.id.keyguard_clock_style_custom,
+            R.id.keyguard_clock_style_custom1,
+            R.id.keyguard_clock_style_custom2,
+            R.id.keyguard_clock_style_custom3
+    };
+
+    private static final int DEFAULT_STYLE = 0; //Disabled
+    private static final String CLOCK_STYLE_KEY = "clock_style";
+
+	private Context mContext;
+	private View[] clockViews;
+	
+	public ClockStyle(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mContext = context;
+	}
+
+	@Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        clockViews = new View[CLOCK_VIEW_IDS.length];
+        for (int i = 0; i < CLOCK_VIEW_IDS.length; i++) {
+            if (CLOCK_VIEW_IDS[i] != 0) {
+                clockViews[i] = findViewById(CLOCK_VIEW_IDS[i]);
+            } else {
+                clockViews[i] = null;
+            }
+        }
+        new MyContentObserver(new Handler()).observe();
+        updateClockView();
+    }
+
+    private void updateClockView() {
+        if (clockViews != null) {
+            int clockStyle = Settings.System.getInt(mContext.getContentResolver(), CLOCK_STYLE_KEY, DEFAULT_STYLE);
+            for (int i = 0; i < clockViews.length; i++) {
+                if (clockViews[i] != null) {
+                    clockViews[i].setVisibility(i == clockStyle ? View.VISIBLE : View.GONE);
+                }
+            }
+        }
+    }
+	
+	class MyContentObserver extends ContentObserver {
+		public MyContentObserver(Handler h) {
+			super(h);
+		}
+		
+		public void observe() {
+			ContentResolver cr = mContext.getContentResolver();
+			cr.registerContentObserver(Settings.System.getUriFor("clock_style"), false, this);
+		}
+		
+		@Override
+		public void onChange(boolean selfChange) {
+			updateClockView();
+		}
+    }
+}

src/com/derp/settings/preference/WallpaperView.kt
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.derp.settings.preference
+
+import android.app.WallpaperManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class WallpaperView : ImageView {
+
+    private val handler = Handler()
+
+    private val wallpaperChecker = object : Runnable {
+        override fun run() {
+            setWallpaperPreview()
+            handler.postDelayed(this, 2000)
+        }
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        setWallpaperPreview()
+        handler.postDelayed(wallpaperChecker, 2000)
+    }
+
+    private fun setWallpaperPreview() {
+        val wallpaperManager = WallpaperManager.getInstance(context)
+        val wallpaperDrawable: Drawable? = wallpaperManager.drawable
+        wallpaperDrawable?.let {
+            val wallpaperBitmap = drawableToBitmap(it)
+            setImageBitmap(wallpaperBitmap)
+        }
+    }
+
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+        val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+
+        return bitmap
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        handler.removeCallbacks(wallpaperChecker)
+    }
+}

Los-Ext/android_packages_apps_Settings

src/com/evolution/settings/preference/ClockStyle.java
@@ -1,0 +1,79 @@
+package com.evolution.settings.preference;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.ContentObserver;
+import android.net.Uri;
+import android.os.Handler;
+import android.provider.Settings;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.RelativeLayout;
+import com.android.settings.R;
+
+public class ClockStyle extends RelativeLayout {
+
+    private static final int[] CLOCK_VIEW_IDS = {
+            R.id.keyguard_clock_style_default,
+            R.id.keyguard_clock_style_oos,
+            R.id.keyguard_clock_style_ios,
+            R.id.keyguard_clock_style_cos,
+            R.id.keyguard_clock_style_custom,
+            R.id.keyguard_clock_style_custom1,
+            R.id.keyguard_clock_style_custom2,
+            R.id.keyguard_clock_style_custom3
+    };
+
+    private static final int DEFAULT_STYLE = 0; //Disabled
+    private static final String CLOCK_STYLE_KEY = "clock_style";
+
+	private Context mContext;
+	private View[] clockViews;
+	
+	public ClockStyle(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		mContext = context;
+	}
+
+	@Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        clockViews = new View[CLOCK_VIEW_IDS.length];
+        for (int i = 0; i < CLOCK_VIEW_IDS.length; i++) {
+            if (CLOCK_VIEW_IDS[i] != 0) {
+                clockViews[i] = findViewById(CLOCK_VIEW_IDS[i]);
+            } else {
+                clockViews[i] = null;
+            }
+        }
+        new MyContentObserver(new Handler()).observe();
+        updateClockView();
+    }
+
+    private void updateClockView() {
+        if (clockViews != null) {
+            int clockStyle = Settings.System.getInt(mContext.getContentResolver(), CLOCK_STYLE_KEY, DEFAULT_STYLE);
+            for (int i = 0; i < clockViews.length; i++) {
+                if (clockViews[i] != null) {
+                    clockViews[i].setVisibility(i == clockStyle ? View.VISIBLE : View.GONE);
+                }
+            }
+        }
+    }
+	
+	class MyContentObserver extends ContentObserver {
+		public MyContentObserver(Handler h) {
+			super(h);
+		}
+		
+		public void observe() {
+			ContentResolver cr = mContext.getContentResolver();
+			cr.registerContentObserver(Settings.System.getUriFor("clock_style"), false, this);
+		}
+		
+		@Override
+		public void onChange(boolean selfChange) {
+			updateClockView();
+		}
+    }
+}

src/com/evolution/settings/preference/WallpaperView.kt
@@ -1,0 +1,90 @@
+/*
+ * Copyright (C) 2023 The risingOS Android Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.evolution.settings.preference
+
+import android.app.WallpaperManager
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
+import android.os.Handler
+import android.util.AttributeSet
+import android.widget.ImageView
+
+class WallpaperView : ImageView {
+
+    private val handler = Handler()
+
+    private val wallpaperChecker = object : Runnable {
+        override fun run() {
+            setWallpaperPreview()
+            handler.postDelayed(this, 2000)
+        }
+    }
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        setWallpaperPreview()
+        handler.postDelayed(wallpaperChecker, 2000)
+    }
+
+    private fun setWallpaperPreview() {
+        val wallpaperManager = WallpaperManager.getInstance(context)
+        val wallpaperDrawable: Drawable? = wallpaperManager.drawable
+        wallpaperDrawable?.let {
+            val wallpaperBitmap = drawableToBitmap(it)
+            setImageBitmap(wallpaperBitmap)
+        }
+    }
+
+    private fun drawableToBitmap(drawable: Drawable): Bitmap {
+        if (drawable is BitmapDrawable) {
+            return drawable.bitmap
+        }
+        val bitmap = Bitmap.createBitmap(
+            drawable.intrinsicWidth,
+            drawable.intrinsicHeight,
+            Bitmap.Config.ARGB_8888
+        )
+        val canvas = Canvas(bitmap)
+        drawable.setBounds(0, 0, canvas.width, canvas.height)
+        drawable.draw(canvas)
+
+        return bitmap
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        handler.removeCallbacks(wallpaperChecker)
+    }
+}